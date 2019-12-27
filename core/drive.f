85f0617ba02bbd0402f103ab5310031f41f71a91
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-12-27 22:25:46 +0530

    Add docstrings to top-level subroutines of NEKTON

--- core/drive.f
+++ core/drive.f
@@ -1,3 +1,12 @@
+c> @defgroup core The Nek5000 core library
+c>
+c> @{
+c
+c> @brief *Drives* the solver calling 3 subroutines:
+c> - nek_init()
+c> - nek_solve()
+c> - nek_end()
+c> - exitt0()
       program NEKTON
 c
       call nek_init(intracomm)
@@ -7,3 +16,5 @@ c
       call exitt0()
 
       end
+c> @}
+c end of group core
