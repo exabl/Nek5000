exabl/Nek5000

commit 8d88e3f2c6f809bca69fda8b2ed1679acc1a4699

Document core solver subroutines

core/drive2.f
@@ -584,7 +584,7 @@ C     Set time step.
 C
       DO 10 ILAG=10,2,-1
          DTLAG(ILAG) = DTLAG(ILAG-1)
- 10   CONTINUE
+10   CONTINUE
       CALL SETDT
       DTLAG(1) = DT
       IF (ISTEP.EQ.1 .and. irst.le.0) DTLAG(2) = DT
@@ -681,6 +681,9 @@ C
       RETURN
       END
 C-----------------------------------------------------------------------
+c> @ingroup core
+c> Driver for solving the incompressible Navier-Stokes equations.
+c> @callgraph
       subroutine fluid (igeom)
 C
 C     Driver for solving the incompressible Navier-Stokes equations.

core/induct.f
@@ -278,6 +278,9 @@ c
       return
       end
 c--------------------------------------------------------------------
+c> Project U onto the closest incompressible field
+c> @ingroup core
+c> @callgraph
       subroutine incomprn (ux,uy,uz,up)
 c
 c     Project U onto the closest incompressible field

core/navier1.f
@@ -1492,6 +1492,13 @@ C
       return
       END
 C
+c> @ingroup core
+c> Compute and add
+c>
+c> 1. user specified forcing function (FX,FY,FZ)
+c> 2. driving force due to natural convection
+c> 3. convection term
+c> @callgraph
       subroutine makef
 C---------------------------------------------------------------------
 C

core/planx.f
@@ -1,3 +1,7 @@
+c> @ingroup core
+c> Compute pressure and velocity using consistent approximation spaces.     
+c> Operator splitting technique.
+c> @callgraph
       SUBROUTINE PLAN3 (IGEOM)
 C-----------------------------------------------------------------------
 C

commit 8a00d90c688f347026599eb91922c67320c3a1bf

fixup! Fix indentation error introduced in 8d88e3f

core/drive2.f
@@ -557,7 +557,7 @@ C     Set time step.
 C
       DO 10 ILAG=10,2,-1
          DTLAG(ILAG) = DTLAG(ILAG-1)
-10    CONTINUE
+ 10   CONTINUE
       CALL SETDT
       DTLAG(1) = DT
       IF (ISTEP.EQ.1 .and. irst.le.0) DTLAG(2) = DT